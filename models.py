88a0022fd670b0d19b1355d20c44f682f16e72fb
Maria Mokbel <mmokbel@octo.com>
2020-03-02 11:16:42 +0100

    Add foreign key mission to consultant table

--- models.py
+++ models.py
@@ -6,14 +6,17 @@ from sqlalchemy.types import Enum
 Base = declarative_base()
 
 
-class Consultant(Base):
-    __tablename__ = 'consultant'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, unique=False, nullable=False)
-
-
 class Mission(Base):
     __tablename__ = 'mission'
     id = Column(Integer, primary_key=True)
     location = Column(String, nullable=False)
     client = Column(String, nullable=False)
+
+
+class Consultant(Base):
+    __tablename__ = 'consultant'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=False, nullable=False)
+    mission_id = Column(ForeignKey('mission.id'),
+                        nullable=False)
+    mission = relationship(Mission)
